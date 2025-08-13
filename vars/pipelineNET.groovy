3bb513f172397cca177afc588be53edfc0660764
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-08-13 15:24:43 -0500

    initial commit

---
+++ vars/pipelineNET.groovy
@@ -0,0 +1,50 @@
+def call(Map config) {
+
+    pipeline {
+        agent {
+            docker {
+                image 'mcr.microsoft.com/dotnet/sdk:8.0'  // La imagen que usarás
+                args '-u root:root'                      // Opcional, para permisos root
+                label 'docker-node'                      // Nodo donde tienes Docker instalado
+            }
+        }
+
+        environment {
+            BUILD_FOLDER = "${env.WORKSPACE}/${env.BUILD_ID}"
+            REPO_PATH = "${BUILD_FOLDER}/repo"
+            REPO_URL = 'https://github.com/IT-HEALTH-PROYECTOS-QC/BACKEND_PROYECTO_QC.git'
+            CONFIGURATION = 'Release'
+            DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = "true"
+        }
+
+        stages {
+            stage('Clone Repository') {
+                steps {
+                    script {
+                        cloneRepo(
+                            branch: env.BRANCH,
+                            repoPath: env.REPO_PATH,
+                            repoUrl: env.REPO_URL
+                        )
+                    }
+                }
+            }
+
+            stage('Load API Config') {
+                steps {
+                    script {
+                        // Cargar el diccionario correspondiente desde resources
+                        def configCompleto = load libraryResource("configs/${config.API_NAME}.groovy")
+                        // Seleccionar el ambiente: demo o test
+                        env.API_CONFIG = configCompleto[config.AMBIENTE]
+
+                        echo "Configuración cargada para API ${config.API_NAME} en ambiente ${config.AMBIENTE}:"
+                        echo "Ruta csproj: ${env.API_CONFIG.rutaCsproj}"
+                        echo "Credenciales: ${env.API_CONFIG.nombreCredenciales}"
+                    }
+                }
+            }
+
+        }
+    }
+}
