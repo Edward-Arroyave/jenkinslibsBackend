9e1cf49cf2cdd273550231955df2f6483a0ab7f8
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-08-19 09:58:43 -0500

    initial commit

---
+++ resources/QC.groovy
@@ -0,0 +1,20 @@
+return [
+    AMBIENTES: [
+        demo: [ BRANCH: "demo" ],
+        test: [ BRANCH: "Test" ]
+    ],
+
+    APIS: [
+        backendQC: [
+            REPO_PATH: "${env.REPO_PATH}/qc_backend_web",
+            CREDENCIALES: [
+                demo: "",
+                test: "QC_BACKEND_TEST"
+            ],
+            URL: [
+                demo: "",
+                test: "https://his-backend-caracterizacion-annar-pruebas.azurewebsites.net"
+            ]
+        ]
+    ]
+]
