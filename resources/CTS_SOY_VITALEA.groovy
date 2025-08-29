7f708d5a357a0daa2ee84a17199a093047662347
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-08-28 22:47:39 -0500

    [fix]: logica para framework 4

---
+++ resources/CTS_SOY_VITALEA.groovy
@@ -0,0 +1,20 @@
+return [
+    AMBIENTES: [
+        test: [ BRANCH: "test" ],
+        demo: [ BRANCH: "demo" ]
+    ],
+
+    APIS: [
+        ApiCrmVitalea: [
+            REPO_PATH: "${env.REPO_PATH}/ApiCrmVitalea",
+            CREDENCIALES: [
+                test: "PROFILE_CTS_VIOLETA_ApiCrmVitalea_TEST",
+                demo: ""
+            ],
+            URL: [
+                test: "http://crm-backend-pruebas.azurewebsites.net",
+                demo: ""
+            ]
+        ]
+    ]
+]
