ca5e790bead032d6f19e184810059f707ba4644e
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-09-02 10:29:59 -0500

    [Refactory]: se modifico el pipeline prinical des acoplando la logica

--- vars/deployDotNetFramework4.groovy
+++ vars/deployDotNetFramework4.groovy
@@ -37,7 +37,7 @@ def call(api, configCompleto, config, CONFIGURATION) {
                     Write-Host "🏗️ Sitio: \$(\$profile.msdeploySite)"
 
                     # Publicar toda la solución
-                    & "${msbuildPath}" "${apiConfig.CS_PROJ_PATH}" `
+                    & "${msbuildPath}" "${env.REPO_PATH}" `
                         /p:DeployOnBuild=true `
                         /p:PublishProfile="\$profile.profileName" `
                         /p:Configuration=${CONFIGURATION} `
