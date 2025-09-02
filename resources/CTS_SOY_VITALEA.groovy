3b4c9b1fdac36221caab3bd69f94fc5e40a0879b
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-09-02 18:50:41 -0500

    [Refactory]: se modifico el pipeline prinical des acoplando la logica

--- resources/CTS_SOY_VITALEA.groovy
+++ resources/CTS_SOY_VITALEA.groovy
@@ -1,20 +1,17 @@
-return [
-    AMBIENTES: [
-        test: [ BRANCH: "test" ],
-        demo: [ BRANCH: "demo" ]
-    ],
-
-    APIS: [
-        ApiCrmVitalea: [
-            REPO_PATH: "${env.REPO_PATH}/ApiCrmVitalea",
-            CREDENCIALES: [
-                test: "PROFILE_CTS_VIOLETA_ApiCrmVitalea_TEST",
-                demo: ""
-            ],
-            URL: [
-                test: "http://crm-backend-pruebas.azurewebsites.net",
-                demo: ""
-            ]
+APIS: [
+    ApiCrmVitalea: [
+        REPO_PATH: "${env.REPO_PATH}/ApiCrmVitalea",
+        LIBRARIES: [
+            VIEWMODELS_PATH: "${env.REPO_PATH}/ViewModels",
+            OUTPUT_DLL: "ApiCrmVitalea/bin/Release/ViewModels.dll"
+        ],
+        CREDENCIALES: [
+            test: "PROFILE_CTS_VIOLETA_ApiCrmVitalea_TEST",
+            demo: ""
+        ],
+        URL: [
+            test: "http://crm-backend-pruebas.azurewebsites.net",
+            demo: ""
         ]
     ]
 ]
