a9c36735c286ddbd79d6de154be72d2a1b670f8b
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-09-04 14:21:21 -0500

    [Refactory]: se modifico el pipeline prinical des acoplando la logica

--- vars/validateApi.groovy
+++ vars/validateApi.groovy
@@ -1,20 +1,23 @@
 def call(String url, String apiName) {
-    echo "🌐 Validando API: ${apiName} en ${url}"
+    stage("🔎 Validar ${apiName}") {
+        echo "🌐 Validando API: ${apiName} en ${url}"
 
-    def response = httpRequest(
-        url: url,
-        validResponseCodes: '100:599', // aceptamos todos, para analizarlos manualmente
-        consoleLogResponseBody: true,
-        timeout: 20
-    )
+        def response = httpRequest(
+            url: url,
+            validResponseCodes: '100:599', // aceptamos todos, para analizarlos manualmente
+            consoleLogResponseBody: true,
+            timeout: 20
+        )
 
-    echo "📡 Respuesta de ${apiName}: código ${response.status}"
+        echo "📡 Respuesta de ${apiName}: código ${response.status}"
 
-    if (response.status >= 500 && response.status <= 599) {
-        error("❌ La API ${apiName} devolvió un error de servidor (código ${response.status})")
-    } else if (response.status >= 400 && response.status <= 499) {
-        echo "⚠️ La API ${apiName} devolvió un error de cliente (código ${response.status}), no se considera error de despliegue"
-    } else {
-        echo "✅ La API ${apiName} está operativa (código ${response.status})"
+        if (response.status >= 500 && response.status <= 599) {
+            error("❌ La API ${apiName} devolvió un error de servidor (código ${response.status})")
+        } else if (response.status >= 400 && response.status <= 499) {
+            echo "⚠️ La API ${apiName} devolvió un error de cliente (código ${response.status}), no se considera error de despliegue"
+            currentBuild.result = 'UNSTABLE'
+        } else {
+            echo "✅ La API ${apiName} está operativa (código ${response.status})"
+        }
     }
 }
