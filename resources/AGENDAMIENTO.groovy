0bb67d34d20ec2f1af0de4a0bf38f9e9aff670de
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-09-11 16:59:37 -0500

    [fix]: error en el despleigue de .netframework 4

--- resources/AGENDAMIENTO.groovy
+++ resources/AGENDAMIENTO.groovy
@@ -14,10 +14,6 @@ return [
             URL: [
                 demo: "https://agendamiento-backend-co-demo-gba4fhbqexf2dzcc.eastus2-01.azurewebsites.net",
                 test: "https://agendamiento-backend-co-pruebas-f9eqcgdtgkeug3cn.eastus2-01.azurewebsites.net"
-            ],
-            WebHook[
-                value: "WEBHOOK_HEALTHBOOK", 
-                chanel: "#desarrollos-backend"
             ]
         ],
         Api_ProcesosAuto: [
