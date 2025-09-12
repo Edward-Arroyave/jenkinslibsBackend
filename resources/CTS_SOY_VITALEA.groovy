5e33d24266682ed6b2690527ee13715fb4a47493
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-09-12 08:56:06 -0500

    [changue se divide el  workespace por ambiente buscano optimizar el tiempo de clonado en minutos ]

--- resources/CTS_SOY_VITALEA.groovy
+++ resources/CTS_SOY_VITALEA.groovy
@@ -1,22 +1,3 @@
-// APIS: [
-//     ApiCrmVitalea: [
-//         REPO_PATH: "${env.REPO_PATH}/ApiCrmVitalea",
-//         LIBRARIES: [
-//             VIEWMODELS_PATH: "${env.REPO_PATH}/ViewModels",
-//             OUTPUT_DLL: "ApiCrmVitalea/bin/Release/ViewModels.dll"
-//         ],
-//         CREDENCIALES: [
-//             test: "PROFILE_CTS_VIOLETA_ApiCrmVitalea_TEST",
-//             demo: ""
-//         ],
-//         URL: [
-//             test: "http://crm-backend-pruebas.azurewebsites.net",
-//             demo: ""
-//         ]
-//     ]
-// ]
-
-
 return [
     AMBIENTES: [
         Test: [ BRANCH: "test" ]
